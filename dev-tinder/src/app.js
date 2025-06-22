5874aea39befe945e7405bd3811d48f024354257
Akash <akash.devstag@gmail.com>
2025-06-22 15:10:11 +0530

    play with route

--- dev-tinder/src/app.js
+++ dev-tinder/src/app.js
@@ -5,12 +5,35 @@ const app = express();
 // app.use((req,res)=> {
 //     res.send("hello form the tester");
 // });
-app.get("/hello",(req,res)=>{
-    res.send({
-        name:"akkash",
-        address:"washim maharashtra"
-    })
-}) // this route only match /hellow 
+
+// app.get("/ab?c", (req, res) => {
+//   res.send({
+//     name: "akkash",
+//     address: "washim maharashtra"
+//   });
+// });
+// app.get("/ab+c", (req, res) => {
+//   res.send({
+//     name: "akkash",
+//     address: "washim maharashtra"
+//   });
+// });
+
+app.get("/user/:useriD", (req, res) => {
+  // console.log(req.query)
+  console.log(req.params)
+  res.send({
+    name: "user1",
+    address: "washim maharashtra"
+  });
+});
+
+// app.get("/hello",(req,res)=>{
+//     res.send({
+//         name:"akkash",
+//         address:"washim maharashtra"
+//     })
+// }) // this route only match /hellow 
 
 // app.use("/hello",(req,res)=> {
 //     res.send("hello with route");
