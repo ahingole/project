6aff2cc6660bdc8228585d4b27d2e09614a6c630
Akash <akash.devstag@gmail.com>
2025-06-22 13:01:09 +0530

    practice get post delete

--- dev-tinder/src/app.js
+++ dev-tinder/src/app.js
@@ -5,10 +5,26 @@ const app = express();
 // app.use((req,res)=> {
 //     res.send("hello form the tester");
 // });
-app.use("/hello",(req,res)=> {
-    res.send("hello with route");
-});
+app.get("/hello",(req,res)=>{
+    res.send({
+        name:"akkash",
+        address:"washim maharashtra"
+    })
+}) // this route only match /hellow 
+
+// app.use("/hello",(req,res)=> {
+//     res.send("hello with route");
+// });
+app.post("/hello",(req,res)=>{
+    console.log("save data on post")//save data to db
+    res.send("data successfully saved")
+})
+
+app.delete("/hello",(req,res)=>{
+console.log("delere")
+res.send("detele successfully")
+})
 
 app.listen(3000,()=>{
-    console.log('listinng the server')
+    console.log('listinng the server 3000')
 })
