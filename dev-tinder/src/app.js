043349248fa859bed386d12aa48e7f8f7676f73e
Akash <akash.devstag@gmail.com>
2025-06-24 20:18:13 +0530

    route handler and query

--- dev-tinder/src/app.js
+++ dev-tinder/src/app.js
@@ -19,34 +19,42 @@ const app = express();
 //   });
 // });
 
-app.get("/user/:useriD", (req, res) => {
-  // console.log(req.query)
-  console.log(req.params)
-  res.send({
-    name: "user1",
-    address: "washim maharashtra"
-  });
-});
+// app.get("/user/:useriD", (req, res) => {
+//   // console.log(req.query)
+//   console.log(req.params)
+//   res.send({
+//     name: "user1",
+//     address: "washim maharashtra"
+//   });
+// });
 
-// app.get("/hello",(req,res)=>{
-//     res.send({
-//         name:"akkash",
-//         address:"washim maharashtra"
-//     })
-// }) // this route only match /hellow 
+app.get("/hello", (req,res, next)=>{
+
+    // res.send({
+    //     name:"akkash",
+    //     address:"washim maharashtra"
+    // });
+  next();
+},
+
+(req,res)=>{ 
+  res.send("this is route2")
+}
+
+) // this route only match /hellow 
 
 // app.use("/hello",(req,res)=> {
 //     res.send("hello with route");
 // });
-app.post("/hello",(req,res)=>{
-    console.log("save data on post")//save data to db
-    res.send("data successfully saved")
-})
+// app.post("/hello",(req,res)=>{
+//     console.log("save data on post")//save data to db
+//     res.send("data successfully saved")
+// })
 
-app.delete("/hello",(req,res)=>{
-console.log("delere")
-res.send("detele successfully")
-})
+// app.delete("/hello",(req,res)=>{
+// console.log("delere")
+// res.send("detele successfully")
+// })
 
 app.listen(3000,()=>{
     console.log('listinng the server 3000')
