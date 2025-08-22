9b7e6befdea92ab2c0506362022a7f349f002029
Akash <akash.devstag@gmail.com>
2025-08-22 14:02:11 +0530

    to add to data bse

--- dev-tinder/src/app.js
+++ dev-tinder/src/app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const { connectDB } = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+//TO  add  express middleware to conevert json into object
+app.use(express.json())
+
+app.post("/signUp",async(req,res)=>{
+  console.log(req.body)
+  const user= new User(req.body)
+try{
+  await user.save();
+  res.send("user added sucessfully...")
+}catch(err){
+  res.status(400).send("not found"+ err.message)
+}
+})
 
 connectDB()
   .then(() => {
